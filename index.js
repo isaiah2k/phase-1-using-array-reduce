56297395c4574c7d0c5a7bf7a46b83a662438c20
Isaiah <zaymgmt707@gmail.com>
2024-07-11 18:35:47 -0700

    updates index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Uses reduce to add everything in batteryBatches array and turn it into a new totalBatteries array
+// Uses reduce to add everything in batteryBatches array and turn it into a new totalBatteries variable
 const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
