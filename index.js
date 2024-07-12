344531c6a3bde8a860ae6dcbf9989e4d7202393b
Isaiah <zaymgmt707@gmail.com>
2024-07-11 18:32:56 -0700

    updates index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Uses reduce to add everything in batteryBatches array and turn it into a new totalBatteries array
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
